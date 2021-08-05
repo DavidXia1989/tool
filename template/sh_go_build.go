36979ffab7a93599ab70320d81929e1be8fa5f01
gaoyanhai <gaoyanhai@shzhanmeng.com>
2021-08-05 19:25:29 +0800

    Update sh_go_build.go

--- template/sh_go_build.go
+++ template/sh_go_build.go
@@ -1,7 +1,7 @@
 package template
 var Sh_go_build = `#!/bin/bash
 export GO111MODULE="on"
-export GOPRIVATE="code.zm.shzhanmeng.com"
-export GOPROXY="https://goproxy.io"
+export GOPRIVATE=code.zm.shzhanmeng.com
+export GOPROXY=https://goproxy.cn
 {{.GoRoot}}/go mod tidy
 {{.GoRoot}}/go build -o {{.Name}} main.go`
