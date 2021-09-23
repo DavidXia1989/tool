e2f6135cd7e5855e6dfc5e3466801e7f4720e51f
xiadawei <xiadwei@shzhanmeng.com>
2021-09-23 10:59:31 +0800

    update

--- microserver/handler.go
+++ microserver/handler.go
@@ -1,65 +1,26 @@
 package template
 
-var Handler = `
-package handler
+var Handler = `package handler
 
 import (
-	"code.zm.shzhanmeng.com/go-common/logging"
-	"fmt"
-	"github.com/micro/go-micro/v2"
-	"github.com/micro/go-micro/v2/registry"
-	"github.com/micro/go-micro/v2/registry/etcd"
-	ratelimit "github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2"
-	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
-	"github.com/opentracing/opentracing-go"
-	"go.uber.org/zap"
-	"{{.Name}}/common"
-	"{{.Name}}/conf"
-	service2 "microServerExample/domain/service"
+	"context"
+	"encoding/json"
+	"{{.Name}}/domain/model"
+	"{{.Name}}/domain/service"
 	"{{.Name}}/proto/example"
 )
 
-var MicroServer micro.Service
+type Example struct {
+	ExampleService service.IExampleService
+}
 
-func RegistryHandler(){
-	//创建链路追踪实例
-
-	t,io,err := common.NewTracer(conf.Conf.Micro.TracerServerName, conf.Conf.Micro.TracerAddr)
+func (e *Example) Login(ctx context.Context,request *example.LoginMsgReq, respose *example.LoginMsgRes) (err error) {
+	example := &model.Example{}
+	jsonRequest,err := json.Marshal(request.Msg[0])
 	if err != nil {
-		logging.ZapLogger.Error(err.Error())
+		return err
 	}
-	defer io.Close()
-	opentracing.SetGlobalTracer(t)
-
-	grpcAddr := conf.Conf.Micro.GrpcAddr + ":" + conf.Conf.Micro.GrpcPort
-
-	MicroServer = micro.NewService(
-		micro.Name(conf.Conf.Micro.ProjectName),
-		micro.Address(grpcAddr),
-		micro.Registry(etcd.NewRegistry(registry.Addrs(conf.Conf.Micro.Registry))),
-		// 限流
-		micro.WrapHandler(ratelimit.NewHandlerWrapper(conf.Conf.Micro.Qps)),
-		// 链路追踪
-		micro.WrapHandler(opentracing2.NewHandlerWrapper(opentracing.GlobalTracer())),
-	)
-
-	MicroServer.Init()
-
-	var errs  []error
-	//注册服务
-	errs = append(errs, example.RegisterExampleHandler(MicroServer.Server(),&Example{
-		ExampleService: service2.NewExampleApi(),
-	}))
-
-	for k:= range errs{
-		if errs[k] !=nil {
-			logging.ZapLogger.Info("业务注册失败",zap.Error(errs[k]))
-			fmt.Println(errs[k].Error())
-		}
-	}
-
-	if err = MicroServer.Run(); err != nil {
-		logging.ZapLogger.Info("micro grpc 启动失败",zap.Error(err))
-		fmt.Println(err)
-	}
+	json.Unmarshal(jsonRequest, example)
+	respose.Result, err = e.ExampleService.Login(example)
+	return nil
 }`
--- microserver/register_handler.go
+++ microserver/register_handler.go
@@ -1,21 +1,53 @@
 package template
 
-var RegisterHandler = `package handler
+var RegisterHandler = `
+package handler
 
 import (
-	"code.zm.shzhanmeng.com/go-common/logging"
-	"fmt"
-	"go.uber.org/zap"
-	service2 "{{.Name}}/domain/service"
-	"{{.Name}}/kernel"
-	"{{.Name}}/proto/example"
+"code.zm.shzhanmeng.com/go-common/logging"
+"fmt"
+"github.com/micro/go-micro/v2"
+"github.com/micro/go-micro/v2/registry"
+"github.com/micro/go-micro/v2/registry/etcd"
+ratelimit "github.com/micro/go-plugins/wrapper/ratelimiter/uber/v2"
+opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
+"github.com/opentracing/opentracing-go"
+"go.uber.org/zap"
+"{{.Name}}/common"
+"{{.Name}}/conf"
+service2 "microServerExample/domain/service"
+"{{.Name}}/proto/example"
 )
 
+var MicroServer micro.Service
+
 func RegistryHandler(){
+	//创建链路追踪实例
+
+	t,io,err := common.NewTracer(conf.Conf.Micro.TracerServerName, conf.Conf.Micro.TracerAddr)
+	if err != nil {
+		logging.ZapLogger.Error(err.Error())
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(t)
+
+	grpcAddr := conf.Conf.Micro.GrpcAddr + ":" + conf.Conf.Micro.GrpcPort
+
+	MicroServer = micro.NewService(
+		micro.Name(conf.Conf.Micro.ProjectName),
+		micro.Address(grpcAddr),
+		micro.Registry(etcd.NewRegistry(registry.Addrs(conf.Conf.Micro.Registry))),
+		// 限流
+		micro.WrapHandler(ratelimit.NewHandlerWrapper(conf.Conf.Micro.Qps)),
+		// 链路追踪
+		micro.WrapHandler(opentracing2.NewHandlerWrapper(opentracing.GlobalTracer())),
+	)
+
+	MicroServer.Init()
+
 	var errs  []error
-	fmt.Println(kernel.MicroServer)
 	//注册服务
-	errs = append(errs, example.RegisterExampleHandler((*kernel.MicroServer).Server(),&Example{
+	errs = append(errs, example.RegisterExampleHandler(MicroServer.Server(),&Example{
 		ExampleService: service2.NewExampleApi(),
 	}))
 
@@ -25,4 +57,10 @@ func RegistryHandler(){
 			fmt.Println(errs[k].Error())
 		}
 	}
+
+	if err = MicroServer.Run(); err != nil {
+		logging.ZapLogger.Info("micro grpc 启动失败",zap.Error(err))
+		fmt.Println(err)
+	}
 }`
+
